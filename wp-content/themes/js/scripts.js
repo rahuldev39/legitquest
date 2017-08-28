dbc9cbd86de63212bd04917a5b4e850c84a312d5
DESKTOP-FOS2UDC\rahul_dev <rahuldev.w@gmail.com>
2017-08-28 19:52:28 +0530

    update(features, index and menu )

--- wp-content/themes/js/scripts.js
+++ wp-content/themes/js/scripts.js
@@ -105,17 +105,16 @@
 
         $( "<span class='mobile-dropdown'></span>" ).appendTo( $( "#menu-main-menu .menu-item-has-children" ) );
 
-        $("#menu-main-menu .menu-item-has-children .mobile-dropdown").click(function() {
+        $("#menu-main-menu .dropdown a").click(function(event) {
         $(this).closest(".menu-item-has-children").toggleClass('mobile-visible');
+        event.preventDefault();
         });
 
 
         // ------------------------------------------------------------------------
         // Main Smooth Scroll and Scroll Spy
         // ------------------------------------------------------------------------
-        $('.navbar-collapse ul li a').click(function() {
-            $('.navbar-toggle:visible').click();
-        });
+        
         $(function() {
             $('.navbar-nav li a, .wpb_wrapper .menu li a').bind('click', function(event) {
                 var $anchor = $(this);
@@ -130,124 +129,7 @@
             target: '.navbar-fixed-top'
         })
     });
-    // ------------------------------------------------------------------------
-    // Particles JS Animation
-    // ------------------------------------------------------------------------
-    if ($("#particles-js").length) {
-        if ($(window).width() > 960) {
-            particlesJS("particles-js", {
-                "particles": {
-                    "number": {
-                        "value": 120,
-                        "density": {
-                            "enable": true,
-                            "value_area": 1800
-                        }
-                    },
-                    "color": {
-                        "value": "#1080f2"
-                    },
-                    "shape": {
-                        "type": "circle",
-                        "stroke": {
-                            "width": 0,
-                            "color": "#000000"
-                        },
-                        "polygon": {
-                            "nb_sides": 3
-                        },
-                        "image": {
-                            "src": "img/github.svg",
-                            "width": 100,
-                            "height": 100
-                        }
-                    },
-                    "opacity": {
-                        "value": 0.5,
-                        "random": false,
-                        "anim": {
-                            "enable": false,
-                            "speed": 1,
-                            "opacity_min": 0.2,
-                            "sync": false
-                        }
-                    },
-                    "size": {
-                        "value": 3,
-                        "random": true,
-                        "anim": {
-                            "enable": false,
-                            "speed": 20,
-                            "size_min": 0.1,
-                            "sync": false
-                        }
-                    },
-                    "line_linked": {
-                        "enable": true,
-                        "distance": 250,
-                        "color": "#909090",
-                        "opacity": 0.2,
-                        "width": 1
-                    },
-                    "move": {
-                        "enable": true,
-                        "speed": 1,
-                        "direction": "none",
-                        "random": false,
-                        "straight": false,
-                        "out_mode": "out",
-                        "bounce": false,
-                        "attract": {
-                            "enable": false,
-                            "rotateX": 600,
-                            "rotateY": 1200
-                        }
-                    }
-                },
-                "interactivity": {
-                    "detect_on": "window",
-                    "events": {
-                        "onhover": {
-                            "enable": false,
-                            "mode": "grab"
-                        },
-                        "onclick": {
-                            "enable": false,
-                            "mode": "push"
-                        },
-                        "resize": true
-                    },
-                    "modes": {
-                        "grab": {
-                            "distance": 180,
-                            "line_linked": {
-                                "opacity": 1
-                            }
-                        },
-                        "bubble": {
-                            "distance": 400,
-                            "size": 40,
-                            "duration": 2,
-                            "opacity": 8,
-                            "speed": 3
-                        },
-                        "repulse": {
-                            "distance": 200,
-                            "duration": 0.4
-                        },
-                        "push": {
-                            "particles_nb": 4
-                        },
-                        "remove": {
-                            "particles_nb": 2
-                        }
-                    }
-                },
-                "retina_detect": true
-            });
 
-        }
-    }
     // ------------------------------------------------------------------------
     // Classie Script
     // ------------------------------------------------------------------------
